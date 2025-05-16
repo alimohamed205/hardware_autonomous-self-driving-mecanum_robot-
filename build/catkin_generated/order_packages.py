a1701b66b51173e2992585aa62c7ea0dddbe4a35
ali_mohamed <aliluka205@gmail.com>
2025-05-16 23:09:04 +0300

    Apply proper .gitignore and clean repo

--- build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = '/home/jetson/project_ws/src'
-whitelisted_packages = ''.split(';') if '' != '' else []
-blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/home/jetson/project_ws/devel;/home/jetson/simulation_ws/devel;/home/jetson/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/home/jetson/project_ws/devel;/home/jetson/simulation_ws/devel;/home/jetson/catkin_ws/devel;/opt/ros/noetic' != '' else []
--- build/mpu_6050_driver/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jetson/project_ws/src/mpu_6050_driver
-# Build directory: /home/jetson/project_ws/build/mpu_6050_driver
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
